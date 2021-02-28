697bba152a77f471f3f0747c72e91238e8e6f357
Philipp <philipphuettig@arcor.de>
2021-02-28 19:20:47 +0100

    add documentation, add curse transform script

---
+++ curseconverter.py
@@ -0,0 +1,32 @@
+import json
+import sys
+
+#TODO: remove
+sys.argv.append("manifest.json")
+sys.argv.append("pack.json")
+
+def main(inFile, outFile):
+    out = []
+    with open(infile) as inF:
+        manifest = json.load(inF)
+        files = manifest["files"]
+        for file in files:
+            out.append({
+                'source': 'curse',
+                'projectID': file["projectID"],
+                'fileID': file["fileID"]
+            })
+
+    with open(outfile, "w") as outF:
+        outObj = {
+            'mods': out
+        }
+        json.dump(outObj, outF, indent=2, sort_keys=True)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("usage: curseconverter.py INFILE OUTFILE")
+        sys.exit(1)
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+    main(infile, outfile)
